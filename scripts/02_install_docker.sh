843d31659c428437475b3338e45974878f6de18b
nanat <nut.prohmpiriya@gmail.com>
2024-12-14 05:34:38 +0700

    install docker

---
+++ scripts/02_install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update the package index
+sudo apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update the package index again
+sudo apt-get update
+
+# Install the latest version of Docker
+sudo apt-get install -y docker-ce
