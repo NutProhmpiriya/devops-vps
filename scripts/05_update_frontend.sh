03ecf15c86bdfe9ca72bbf8a3f3252cb0cde61b6
nanat <nut.prohmpiriya@gmail.com>
2024-12-14 05:30:10 +0700

    fix ....

--- scripts/05_update_frontend.sh
+++ scripts/05_update_frontend.sh
@@ -1,10 +1,9 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 
-# # Pull the latest Docker image for the frontend
-# docker pull $DOCKERHUB_USERNAME/frontend:latest
+source ../.env
+echo "DOCKERHUB_USERNAME: $DOCKERHUB_USERNAME"
+# Pull the latest Docker image for the frontend
+docker pull $DOCKERHUB_USERNAME/frontend:latest
 
-# # Run the Docker image for the frontend
-# docker run -d --name frontend-container $DOCKERHUB_USERNAME/frontend:latest
-
-touch textfile.txt
-echo "create file textfile.txt"
+# Run the Docker image for the frontend
+docker run -d --name frontend-container $DOCKERHUB_USERNAME/frontend:latest
