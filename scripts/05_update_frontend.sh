5ec750c2833eb7ca111c68511b5ebb0d0714793c
nanat <nut.prohmpiriya@gmail.com>
2024-12-14 03:25:00 +0700

    fix script

---
+++ scripts/05_update_frontend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# # Pull the latest Docker image for the frontend
+# docker pull $DOCKERHUB_USERNAME/frontend:latest
+
+# # Run the Docker image for the frontend
+# docker run -d --name frontend-container $DOCKERHUB_USERNAME/frontend:latest
+
+touch textfile.txt
